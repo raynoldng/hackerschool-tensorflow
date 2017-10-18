ac21b8a09cc731592e68dc4f8726b3ed8c863d67
Raynold Ng <raynold.ng24@gmail.com>
2017-10-19 00:12:30 +0800

    First commit

---
+++ slides/slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'Introduction to TensorFlow', 
+     subtitle: 'Hackerschool, NUS Hackers', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/tensorflow.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'Raynold Ng', 
+     company: '', 
+     gplus: '', 
+     twitter: '@nushackers', 
+     www: 'http://www.nushackers.org/', 
+     github: '', 
+   }]
+};
